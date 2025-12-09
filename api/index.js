60d6f5a8a30968e9e04f7b9551144b253d26194d
sadar786 <bathoorsadarullah@gmail.com>
2025-12-09 09:29:56 +0500

    edded for vercel 3

--- api/index.js
+++ api/index.js
@@ -3,6 +3,12 @@ const app = require("../server");
 
 const handler = serverless(app);
 
-module.exports = (req, res) => {
+// Vercel Node.js runtime expects a default export
+module.exports = async (req, res) => {
+  return handler(req, res);
+};
+
+// ALSO export as default for safety
+exports.default = async (req, res) => {
   return handler(req, res);
 };
